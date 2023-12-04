377a34fd67bd6b5641d3f1ee0d330caa18502bfc
Gayathri AV <gaya3kousthubam@gmail.com>
2023-12-04 19:00:46 +0530

    footer font

--- contact.js
+++ contact.js
@@ -12,22 +12,7 @@ class ContactData extends HTMLElement {
         let city = data.city.get();
         let message = data.message.get();
 
-        const shadow = this.shadowRoot;
-        const div = document.createElement('div');
-        div.textContent = numberToWords(num);
-        div.style.color = 'orange';
-        div.style.borderWidth = '1px';
-        div.style.borderColor = 'yellow';
-        div.style.borderStyle = 'dashed';
-        div.style.padding = '10px';
-
-        // `on_change()` method listen to any changes done
-        // against the argument value in ftd.
-        data.num.on_change(function () { 
-            const changed_value = data.num.get();  
-            div.textContent = numberToWords(changed_value);  
-        }) 
-
-        shadow.appendChild(div);
     }
 }
+
+customElements.define('contact-data', ContactData);
